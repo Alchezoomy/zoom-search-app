c59879ff3f5fac772dce91e0bee100cfa9ee3b79
dl-watson <dlarkinwatson@gmail.com>
2020-11-15 12:48:09 -0800

    testing this cute-ass about-us page with zoom recording

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class AboutUs extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        // zero styling
+        <video autoPlay="true">
+          <source src="/images/zoomies.webm" type="video/webm" />
+        </video>
+      </div>
+    );
+  }
 }
