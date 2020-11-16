e5a8143d1eab0d3128bc3ed00b0aa1b8744d2ee0
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-11-15 17:20:55 -0800

    redirect teacher page test

--- src/Teacher.js
+++ src/Teacher.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
 export default class Teacher extends Component {
+    state = {
+        loading: true,
+        returnedObject: {}
+    }
+
+    componentDidMount = async () => {
+        const URL = 'https://alchezoomy.herokuapp.com/oauth';
+        try {
+            const returnedObject = await fetch.post(URL).send({ token: this.props.baseState.code });
+            this.setState({
+                returnedObject: returnedObject.body,
+                loading: false
+            })
+        } catch(e) {
+            throw e;
+        }
+    }
     render() {
         return (
             <div className='teacher'>
-                
+                {this.state.loading
+                ? <img src='/loading spinner.gif' alt='loading spinner' />
+                : <p>test passed</p>
+                }
             </div>
         )
     }
