a0d52f770779b76681cfd24cb7524e6fefc95681
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-11-17 14:38:45 -0800

    worked on favorites, add privateroutes, various stuff

--- src/Favorites.js
+++ src/Favorites.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import { fetchFavorites } from './Fetches'
+import DashMenu from './DashMenu.js'
 
 export default class Favorites extends Component {
+    state = {
+        loading: true,
+        favorites: [],
+        search: '',
+    }
+    componentDidMount = async () => {
+        const favorites = await fetchFavorites(this.props.token)
+        this.setState({
+            favorites: favorites,
+        })
+    }
     render() {
         return (
+
             <div>
-                
+                <div className='left-nav'>
+                    <DashMenu
+                    />
+                </div>
+                {this.state.favorites.map(video =>
+                    <div>
+
+                        <div> {video.uuid} </div>
+
+                    </div>
+
+
+                )
+                }
             </div>
         )
     }
