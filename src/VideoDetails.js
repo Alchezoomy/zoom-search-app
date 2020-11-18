2fdae90fd406391617e856943da62f8fc3d63482
dl-watson <dlarkinwatson@gmail.com>
2020-11-18 14:28:24 -0800

    preserving work on fuzzy searching before pulling from main

--- src/VideoDetails.js
+++ src/VideoDetails.js
@@ -1,86 +1,121 @@
-import React, { Component } from 'react'
-import DashMenu from './DashMenu.js'
-import Player from './Player.js'
+import React, { Component } from "react";
+import "./App.css";
+import DashMenu from "./DashMenu.js";
+import Player from "./Player.js";
+import Fuse from "fuse.js";
 import {
-
-    fetchVideo,
-    favoriteVideo,
-    fetchTranscript, 
-    fetchChat
-
-} from './Fetches.js';
+  fetchVideo,
+  favoriteVideo,
+  fetchTranscript,
+  fetchChat,
+} from "./Fetches.js";
 
 export default class VideoDetails extends Component {
+  state = {
+    loading: true,
+    video: [],
+    search: "",
+    transcript: [],
+    chats: [],
+    fuzzy: "",
+    query: "",
+  };
 
-    state = {
-        loading: true,
-        video: [],
-        search: '',
-        transcript: [],
-        chats: []
-    }
+  componentDidMount = async () => {
+    const video = await fetchVideo(
+      this.props.match.params.id,
+      this.props.token
+    );
 
-    componentDidMount = async () => {
-        const video = await fetchVideo(this.props.match.params.id, this.props.token);
+    const transcript = await fetchTranscript(
+      this.props.match.params.id,
+      this.props.token
+    );
 
-        const transcript = await fetchTranscript(this.props.match.params.id, this.props.token);
+    const chats = await fetchChat(this.props.match.params.id, this.props.token);
 
-        const chats = await fetchChat(this.props.match.params.id, this.props.token);
+    this.setState({
+      video: video,
+      transcript: transcript,
+      chats: chats,
+    });
+  };
 
-        this.setState({
-            video: video,
-            transcript: transcript,
-            chats: chats,
-        })
-    }
+  handleFavorite = async (e) => {
+    const newFavorite = {
+      uuid: 10,
+      host_id: this.state.video.host_id,
+      topic: this.state.video.topic,
+      start_time: 4,
+      timestamp: 4,
+      speaker: "",
+      text: "",
+      owner_id: this.state.video.owner_id,
+    };
+    await favoriteVideo(newFavorite, this.props.token);
+  };
 
+  handleSearch = async (e) => {
+    const { transcript } = this.state;
 
-    handleFavorite = async (e) => {
+    const options = {
+      includeScore: true,
+      shouldSort: true,
+      ignoreLocation: true,
+      threshold: 0.2,
+      keys: ["text"],
+    };
 
-        const newFavorite = {
-            uuid: 10,
-            host_id: this.state.video.host_id,
-            topic: this.state.video.topic,
-            start_time: 4,
-            timestamp: 4,
-            speaker: "",
-            text: "",
-            owner_id: this.state.video.owner_id
+    const fuse = new Fuse(transcript, options);
+    const fuzzysearch = fuse.search(e.target.value);
 
-        }
-        await favoriteVideo(newFavorite, this.props.token);
+    this.setState({
+      fuzzy: fuzzysearch,
+      query: e.target.value,
+    });
 
-    }
-    render() {
-        return (
-            <div className='video-details'>
+    console.log(this.state.fuzzy);
+  };
 
-                <div className='left-nav'>
-                    <DashMenu
-                    />
-                </div>
-                <h3 className='video-header'>{this.state.video.topic}</h3>
-                <div className='video-detail'>
-                    <div className='video'>
-                        <Player
-                            video_url={this.state.video.video_play_url} />
-
-                    </div>
-                </div>
-                <div className='buttons'>
-                    <button onClick={this.handleFavorite} className='favorite-button'>Favorite</button>
-                    <button className="bookmarks">Bookmark Timestamp</button>
-                </div>
-
-                <div className='transcript'>{this.state.transcript.map(trans =>
-                        <div>{trans.text}</div>
-                    )}
-                    </div>
-                    <div className='chat'>{this.state.chats.map(chat =>
-                        <div>{chat.timestamp} {chat.speaker} {chat.text}</div>
-                    )}
-                    </div>
+  render() {
+    const { transcript, chats, video, fuzzy } = this.state;
+    return (
+      <div className="video-details">
+        <input onChange={(e) => this.handleSearch(e)} />
+        <div className="left-nav">
+          <DashMenu />
+        </div>
+        <h3 className="video-header">{video.topic}</h3>
+        <div className="video-detail">
+          <div className="video">
+            <Player video_url={video.video_play_url} />
+          </div>
+        </div>
+        <div className="buttons">
+          <button onClick={this.handleFavorite} className="favorite-button">
+            Favorite
+          </button>
+          <button className="bookmarks">Bookmark Timestamp</button>
+        </div>
+        <div className="transcript">
+          {transcript.map((trans) => {
+            fuzzy.map((match) => {
+              if (match.item?.text === trans.text) {
+                return <div className="highlight-me">{trans.text}</div>;
+              } else {
+                return <div>{trans.text}</div>;
+              }
+            });
+          })}
+        </div>
+        <div className="chat">
+          {chats.map((chat) => (
+            <div>
+              {chat.timestamp} {chat.speaker} {chat.text}
             </div>
-        )
-    }
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
